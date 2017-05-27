0d00332b4eb34b09f5f23325fff95f2a8f15aa84
Adeyinka Alabi <adeyinka.alabi@andela.com>
2017-05-27 21:28:00 +0100

    chore (server): setup server and babel [Delivers #146206505]

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import http from 'http';
+
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+
+// Set up the express app
+const app = express();
+
+app.set('port', port);
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default all-route that sends back a welcome message in JSON format.
+app.get('/*', (req, res) =>
+  res.status(200).send('Welcome to PAPYRUS'));
+
+const server = http.createServer(app);
+server.listen(port);
+/* eslint-disable */
+console.log(`server started on port ${port}`);
+/* eslint-enable */
