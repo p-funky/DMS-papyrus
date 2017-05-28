ec429f87e0ce81a63317bfd27051c5e042ef3848
Adeyinka Alabi <adeyinka.alabi@andela.com>
2017-05-28 13:25:13 +0100

    chore: configure webpack [delivers #144851289]

--- server.js
+++ server.js
@@ -2,15 +2,30 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import http from 'http';
+import path from 'path';
+
+
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from './webpack.config.dev';
 
 import documents from './server/routes/documentsRoutes';
 import user from './server/routes/userRoutes';
 
 
 const port = parseInt(process.env.PORT, 10) || 8000;
+const compiler = webpack(webpackConfig);
 
 // Set up the express app
 const app = express();
+
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
 
 app.set('port', port);
 
