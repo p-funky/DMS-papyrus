976fba6058c3b74cce2b68c556f0d154bd5ff528
Adeyinka Alabi <adeyinka.alabi@andela.com>
2017-05-30 12:08:12 +0100

    make input fields required

--- client/components/signIn/signInForm.jsx
+++ client/components/signIn/signInForm.jsx
@@ -38,14 +38,14 @@ class SignInForm extends React.Component {
               <div className="row">
                 <div className="input-field col s12">
                   <i className="material-icons prefix">input</i>
-                  <input id="credential" type="text" className="validate" onChange={this.onChange} />
+                  <input id="credential" type="text" className="validate" required onChange={this.onChange} />
                   <label htmlFor="username|email">username or email</label>
                 </div>
               </div>
               <div className="row">
                 <div className="input-field col s12">
                   <i className="material-icons prefix">lock</i>
-                  <input id="password" type="password" className="validate" onChange={this.onChange} />
+                  <input id="password" type="password" className="validate" required onChange={this.onChange} />
                   <label htmlFor="password">password</label>
                 </div>
               </div>
--- client/components/signup/signupForm.jsx
+++ client/components/signup/signupForm.jsx
@@ -29,42 +29,42 @@ class SignupForm extends React.Component {
               <div className="row">
                 <div className="input-field col s12">
                   <i className="material-icons prefix">perm_identity</i>
-                  <input id="firstName" type="text" className="validate" onChange={this.onChange} />
+                  <input id="firstName" type="text" className="validate" required onChange={this.onChange} />
                   <label htmlFor="firstname">first name</label>
                 </div>
               </div>
               <div className="row">
                 <div className="input-field col s12">
                   <i className="material-icons prefix">perm_identity</i>
-                  <input id="lastName" type="text" className="validate" onChange={this.onChange} />
+                  <input id="lastName" type="text" className="validate" required onChange={this.onChange} />
                   <label htmlFor="lastname">last name</label>
                 </div>
               </div>
               <div className="row">
                 <div className="input-field col s12">
                   <i className="material-icons prefix">account_circle</i>
-                  <input id="userName" type="text" className="validate" onChange={this.onChange} />
+                  <input id="userName" type="text" className="validate" required onChange={this.onChange} />
                   <label htmlFor="username">username</label>
                 </div>
               </div>
               <div className="row">
                 <div className="input-field col s12">
                   <i className="material-icons prefix">email</i>
-                  <input id="email" type="email" className="validate" onChange={this.onChange} />
+                  <input id="email" type="email" className="validate" required onChange={this.onChange} />
                   <label htmlFor="email">email</label>
                 </div>
               </div>
               <div className="row">
                 <div className="input-field col s12">
                   <i className="material-icons prefix">lock</i>
-                  <input id="password" type="password" className="validate" onChange={this.onChange} />
+                  <input id="password" type="password" className="validate" required onChange={this.onChange} />
                   <label htmlFor="password">password</label>
                 </div>
               </div>
               <div className="row">
                 <div className="input-field col s12">
                   <i className="material-icons prefix">lock</i>
-                  <input id="password_confirm" type="password" className="validate" onChange={this.onChange} />
+                  <input id="password_confirm" type="password" className="validate" required onChange={this.onChange} />
                   <label htmlFor="password">confirm password</label>
                 </div>
               </div>
